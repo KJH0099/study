fad1ff881d8dfb8a1749c056c274a54ff149fe03
KJH0099 <jh0099j@gmail.com>
2022-02-07 13:59:03 +0900

    날씨 api 적용 html 날씨div 추가 js 코드작성

--- momentum/js/weather.js
+++ momentum/js/weather.js
@@ -1,13 +1,23 @@
 
+const API_KEY = "a20ad7d2707ca4dcf8f27e9d4e467b67";
+
 function onGeoOk(position){ 
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-     console.log(lat , lng);
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+    fetch(url)
+        .then((response) => response.json()
+        .then(data => {
+        console.log(data.name,data.weather[0].main)
+        const weather = document.querySelector("#weather span:first-child")
+        const city = document.querySelector("#weather span:last-child")
+        city.innerText = data.city;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+
+    }));
 }
 
 function onGeoEr(){
     alert("Error!");
 }
 
-navigator.geolocation.getCurrentPosition(onGeoOk,onGeoEr);
-onGeoOk({});
