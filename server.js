ee8153c2af0f62f4f0b9c3f3682297ca8ffddb85
KJH0099 <jh0099j@gmail.com>
2022-04-19 00:31:28 +0900

    server test

---
+++ server.js
@@ -0,0 +1,10 @@
+// node_modules에 있는 express 관련 파일을 가져옴
+const express = require('express');
+
+// express 함수 변환값 변수에 저장
+const app = express();
+
+// 8000 포트로 서버오픈
+app.listen(8000,function(){
+    console.log('server start on 8000')
+});
