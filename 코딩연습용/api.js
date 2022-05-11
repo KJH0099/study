367e3694943920a99ffb7334fc0c4548280485b2
Kim Jihyeon <jh0099j@gmail.com>
2022-05-11 18:54:35 +0900

    kakao map api 사용연습

--- 코딩연습용/api.js
+++ 코딩연습용/api.js
@@ -1,1 +1,6 @@
-let conttainer = document.querySelector('map'); // 지도를 담을 영여의 dom 레퍼런스 
+let container = document.getElementById('map'); // 지도를 담을 영여의 dom      레퍼런스 
+let options = {//지도를 생성할 때 필요한 기본 옵션
+  center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표. 지도를      생성하는데 반드시 필요 
+  level: 25 //지도의 레벨(확대, 축소 정도) } 
+} 
+ let map = new kakao.maps.Map(container,options);
